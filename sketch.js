420f52bcd97629ac09cfc182b77d7ad86d86a2c9
qwrtsdev <potatoloadzch@gmail.com>
2025-09-21 16:18:18 +0700

    Added sudoku grid lines

--- sketch.js
+++ sketch.js
@@ -4,4 +4,18 @@ function setup() {
 
 function draw() {
     background(220);
+
+    // draw tiny grids with thin lines
+    strokeWeight(1);
+    for (let i = 1; i < 9; i++) {
+        line((i * width) / 9, 0, (i * width) / 9, height);
+        line(0, (i * height) / 9, width, (i * height) / 9);
+    }
+
+    // draw 3*3 grids with thick lines
+    strokeWeight(2);
+    line(width / 3, 0, width / 3, height);
+    line((2 * width) / 3, 0, (2 * width) / 3, height);
+    line(0, height / 3, width, height / 3);
+    line(0, (2 * height) / 3, width, (2 * height) / 3);
 }
